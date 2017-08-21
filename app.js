cb4e1444fd397684a9c9e5cd265b48ba99bc3df3
lishm <18679112211@163.com>
2017-08-21 10:58:04 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require("express-session");
+var settings = require("./settings");
+var MongoStore = require("connect-mongo")(session);
+var flash = require('connect-flash');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express(); 
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
+app.use(session({ 
+	secret:settings.cookieSecret,
+	store:new MongoStore({
+		db:settings.db,
+		url:'mongodb://localhost/microblog'
+	}),
+	resave:true,
+	saveUninitialized:true
+}));
+
+
+
+//获取状态
+app.use(function(req,res,next){
+    console.log("app.usr local");
+    res.locals.user = req.session.user;
+  //  res.locals.post = req.session.post;
+    var error = req.flash('error');
+    res.locals.error = error.length?error:null;
+
+    var success = req.flash('success');
+    res.locals.success = success.length?success:null;
+
+    next();
+});
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ settings.js
@@ -0,0 +1,5 @@
+module.exports = {
+	cookieSecret:'microblogbylishm',
+	db:'microblog',
+	host:'localhost'
+}
